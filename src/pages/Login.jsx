be5847540f5345bbfa3230ed89b5f92f8af42d90
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-02-16 12:53:55 -0300

    alterando botão

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -73,7 +73,7 @@ class Login extends Component {
             >
               <AiFillSetting />
               {'  '}
-              Configurações
+              Settings
             </button>
           </form>
         </div>
